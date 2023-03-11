90b18fcbfe95e51aceacec76f5b6ff3416351a3d
KUCAK18 <KUCAK18@DESKTOP-2E6PG8N>
2023-03-11 03:10:36 +0300

    box on added

--- dimensionalPlotSettings.m
+++ dimensionalPlotSettings.m
@@ -1,4 +1,4 @@
 function dimensionalPlotSettings()
 xlabel("r (mm)","FontSize",15);
-ylabel("V(m/s)","FontSize",15)
+ylabel("V(m/s)","FontSize",15); box on;
 end
--- nondimensionalPlotSettings.m
+++ nondimensionalPlotSettings.m
@@ -1,4 +1,4 @@
 function nondimensionalPlotSettings()
 xlabel("r/R","FontSize",15);
-ylabel("V/V_m_a_x","FontSize",15)
+ylabel("V/V_m_a_x","FontSize",15); box on;
 end
