36a72efc7d96c960bf7bc0060a7fcbcb8a2a9e21
KAGAN UCAK <kucak18@ku.edu.tr>
2023-04-01 14:55:01 +0300

    contour plots are done, and color maps are adjusted

--- impellarRegionAdjustmentV2.m
+++ impellarRegionAdjustmentV2.m
@@ -1,7 +1,7 @@
 %% Impellar passage region relative velocity plotting
 % Last edit: 27.03.2023, Created 26.03.2023, 
 clear; clc; close all
-
+clrMap = load("colorMap2.mat");
 angularVelocity = 15.6; %hz
 rotationAngles = [0 -11 -17 -50 -68 -73];
 z = 0;
@@ -14,7 +14,7 @@ elseif z == 2
 elseif z == 3
     xShift= -1; yShift = -16; axisArray = [-5 30 -27 10];
 elseif z == 6
-    xShift= -3; yShift = 7; axisArray = [-5 30 -10 28];
+    xShift= -3; yShift = 7; axisArray = [-5 30 -6 28];
 elseif z == 5
     xShift= -1; yShift = 0; axisArray = [-5 30 -13 25];
 else
@@ -24,6 +24,7 @@ myTable = table2array(readtable("./_vectorFieldImpellarInside/Export_Date=230308
 img = imread('fdaRotorimage.png');
 img =imrotate(img,rotationAngles(z),"bilinear","crop");
 f1 = figure;
+set(gcf, 'WindowState', 'maximized'); pause(0.2)
 subplot(1,2,1)
 imageR = 26.5;
 image('CData',img,'XData',[-imageR imageR],'YData',[-imageR imageR])
@@ -43,7 +44,7 @@ for i=1:length(myTable)
     r = sqrt(x^2+y^2);
     contourTable(i,:) = [x,y,0,0];
     newTable(i,:) = [x,y,0,0];
-    if r < 26 && r>11.75 && (myTable(i,3) ~=0 || myTable(i,4) ~=0)
+    if r < 25 && r>11.75 && (myTable(i,3) ~=0 || myTable(i,4) ~=0)
         bladeVelocityMagnitude = angularVelocity*r/1000*2*pi;
         angle = atan2(y,x);
         myTable(i,3) = myTable(i,3)/xScale*ourScale+bladeVelocityMagnitude*sin(angle);
@@ -63,31 +64,40 @@ axis equal;axis(axisArray)
 title("Angle " +k);
 
 %% single window
-figure 
-image('CData',img,'XData',[-imageR imageR],'YData',[-imageR imageR])
-hold on
-arrowDensity = 1;
-quiver(newTable(1:arrowDensity:end,1),newTable(1:arrowDensity:end,2),newTable(1:arrowDensity:end,3),newTable(1:arrowDensity:end,4),2,"r","LineWidth",1);
-axis equal;axis(axisArray)
-title("Angle " +k);
+arrowDensity = 2;
+% figure 
+% image('CData',img,'XData',[-imageR imageR],'YData',[-imageR imageR])
+% hold on
+% quiver(newTable(1:arrowDensity:end,1),newTable(1:arrowDensity:end,2),newTable(1:arrowDensity:end,3),newTable(1:arrowDensity:end,4),2,"r","LineWidth",1);
+% axis equal;axis(axisArray)
+% title("Angle " +k);
+
 writematrix(newTable,"angle"+k+"VectorField.txt","Delimiter","tab")
 
-
 %% contour
-figure
+f2 = figure;
+set(gcf, 'WindowState', 'maximized'); pause(0.2)
 x2 = unique(myTable(:,1),'rows');
 y2 = unique(myTable(:,2),'rows');
 z2 = sqrt(contourTable(:,3).^2+contourTable(:,4).^2);
+indx = find(z2>1);
+z2(indx) = 1;
 Z = reshape(z2,161,101)';
 
-contourf(x2(1:end)+xShift,-y2(1:end)+yShift,Z,10,"LineColor","none")
+contourf(x2(1:end)+xShift,-y2(1:end)+yShift,Z,30,"LineColor","none")
 axis equal
-colorbar; new_cmap = jet(64); colormap(new_cmap)
+colorbar; new_cmap = jet(32); 
+colormap(clrMap.CustomColormap2); 
+% colormap(new_cmap);
+clim([0 .5])
 
 hold on; 
-quiver(newTable(1:arrowDensity:end,1),newTable(1:arrowDensity:end,2),newTable(1:arrowDensity:end,3),newTable(1:arrowDensity:end,4),2,"k","LineWidth",1);
+quiver(newTable(1:arrowDensity:end,1),newTable(1:arrowDensity:end,2),newTable(1:arrowDensity:end,3),newTable(1:arrowDensity:end,4),2.5,"k","LineWidth",1);
 axis equal;axis(axisArray)
 title("Angle " +k);
+pause(1);
+saveas(f2,".\contourPlots\contourAngle"+k+".png");
+
 end
 
 
